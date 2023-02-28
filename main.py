f9400e7a6a61030dbb051089fd7362b9ea167e62
Kamran Najafzade <nadjafzadeh@gmail.com>
2023-02-28 22:35:33 +0400

    Initial commit

---
+++ main.py
@@ -0,0 +1,11 @@
+def flatten(S):
+    if S == []:
+        return S
+    if isinstance(S[0], list):
+        return flatten(S[0]) + flatten(S[1:])
+    return S[:1] + flatten(S[1:])
+
+
+original_list = [[[1, 2], 3], [4], [5, 6], [7, [8, 9]]]
+print('Original List1', original_list)
+print('Transformed Flat List1', flatten(original_list))
